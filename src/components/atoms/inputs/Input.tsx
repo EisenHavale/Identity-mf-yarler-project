6c9b17136c01b1d9a7c1d5040be0014993837510
Eisen Havale <eisenprogramando@gmail.com>
2022-10-25 14:57:22 -0500

    In process to get second test for inputs

--- src/components/atoms/inputs/Input.tsx
+++ src/components/atoms/inputs/Input.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import {TextField} from '@mui/material';
 import { IInputProps } from '../../../interfaces/atoms-interfaces';
-import './Input.css';
+// import './Input.css';
 
 export const Input = ({label, type, onChange, name, id, error, errorText, color}: IInputProps) => {
-  //TODO avoid textFields get bigger when showing error messages 
+  /* 
+  TODO avoid textFields get bigger when showing error messages 
+  TODO Add a password reveler 
+  ¨*/
   return (
     <TextField 
       label={label} 
-      type={type} 
+      type={type!='select' ? type : ''} 
+      select={type=='select' ? true :false}
       onChange={onChange}
       name={name} 
       id={id} 
--- src/components/atoms/inputs/test/Input.test.tsx
+++ src/components/atoms/inputs/test/Input.test.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import {cleanup, render, screen} from '@testing-library/react'
 import {Input} from '../Input'
 import { act } from 'react-dom/test-utils';
-import { unmountComponentAtNode } from 'react-dom';
+
 
 
 describe('Input simple tests to verify it is functional', () => {
   afterEach(cleanup); 
   
   it('Should render a simple input with label, id, name and onChange event handler', ()=>{
-      const input = <Input label='Name' name='first' value='' onChange={()=>{console.log('Hello')}} />
+      const input = <Input color='info' error={false} label='Name' name='first' value='' onChange={()=>{console.log('Hello')}} />
       act(()=>{
         render(input)
       });
@@ -19,4 +19,25 @@ describe('Input simple tests to verify it is functional', () => {
       expect(renderedInput?.getAttribute('name')).toBe('first');
       expect(labelText.length).toBe(2);
   })
+  it('should render main types of input, number, text and checkbox', () => {
+    const numberInput = <Input type='number' color='info' error={false} label='Phone' name='celphone' value='' onChange={()=>{console.log('Hello')}} />
+    const textInput = <Input type='text' color='info' error={false} label='Name' name='first' value='' onChange={()=>{console.log('Hello')}} />
+    const passwordInput = <Input type='password' color='info' error={false} label='Password' name='password' value='' onChange={()=>{console.log('Hello')}} />
+    const selectInput = <Input type='select' color='info' error={false} label='Select' name='select' value='' onChange={()=>{console.log('Hello')}} />
+    act(()=>{
+      render(numberInput);
+    })
+    const number = screen.getByRole('textbox',{name:'celphone'})
+    expect(number.getAttribute('type')).toBe('number');
+    act(()=>{
+      render(textInput);
+    })
+    act(()=>{
+      render(passwordInput);
+    })
+    act(()=>{
+      render(selectInput);
+    })
+  })
 });
+
